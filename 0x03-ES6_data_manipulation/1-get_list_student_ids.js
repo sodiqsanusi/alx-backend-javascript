883b48d4e4c9c3f10c0153150a6457154a585092
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-01 17:35:58 +0000

    task: use the 'map' array function to get information from passed data

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds (violet) {
+  let lilac = [];
+  if (!Array.isArray(violet)) {
+    return (lilac);
+  }
+  lilac = violet.map(person => person.id);
+  return (lilac);
+}
