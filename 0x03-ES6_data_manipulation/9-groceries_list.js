e76daf893638e5530c915f9c4b075b40553c3734
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-07-02 12:15:27 +0100

    task: use a Map structure to represent given data

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const lilacMap = new Map();
+
+  lilacMap.set('Apples', 10);
+  lilacMap.set('Tomatoes', 10);
+  lilacMap.set('Pasta', 1);
+  lilacMap.set('Rice', 1);
+  lilacMap.set('Banana', 5);
+
+  return (lilacMap);
+}
